76296ed8e2f5bd3614e64677c04ea027d32cda3c
limars874 <limars874@gmail.com>
2016-01-20 23:37:19 +0800

    测试onPress属性及回调函数

--- layoutTest02.js
+++ layoutTest02.js
@@ -6,6 +6,9 @@
  还有个子元素定位的两个属性：alignItems（flex-start | flex-end | center | baseline | stretch）负责水平方向，justfiContent（flex-start | flex-end | center | space-between | space-around
  ）负责竖直方向。
 
+ 顺便测试了一下图片嵌套文字（可行），或者文字上面盖图片(不要这么干，一切都乱套了)，以及图片上再盖图片(透明png盖上去没问题）
+
+
 
  **/
 
@@ -18,6 +21,7 @@ var {
     View,
     Text,
     StyleSheet,
+    Image,
     } = React;
 
 
@@ -44,10 +48,28 @@ var layoutTest02 = React.createClass({
 
                 </View>
                 <View style = {{flex:10,borderWidth:1,borderColor:'blue'}}>
-                    <Text style={{marginTop:40,fontSize:25}}>1/2</Text>
-                    <View style={[styles.viewcenter,styles.viewborder]}><Text>center</Text></View>
-                    <View style={[styles.viewstart,styles.viewborder]}><Text>start</Text></View>
-                    <View style={[styles.viewend,styles.viewborder]}><Text>end</Text></View>
+                    <Text style={{marginTop:5,fontSize:25}}>1/2</Text>
+                    <View style={[styles.viewcenter,styles.viewborder]}>
+                        <Image style={[styles.img,]} source={{uri:'http://p0.meituan.net/mmc/fe4d2e89827aa829e12e2557ded363a112289.png'}}>
+                            <Text style={{marginTop:20,}}>center</Text>
+                        </Image>
+                    </View>
+
+                    <View style={[styles.viewstart,styles.viewborder]}>
+                        <Text style={{marginTop:0,width:100,}}>start
+                            <Image style={{height:30,width:30,}} source={{uri:'http://p0.meituan.net/mmc/fe4d2e89827aa829e12e2557ded363a112289.png'}}>
+                            </Image>
+                        </Text>
+
+                    </View>
+
+                    <View style={[styles.viewend,styles.viewborder]}>
+                        <Image style={{width:120,height:80,}} source={{uri:'http://p1.meituan.net/200.0/deal/c3f56d253b6481ca15b5075b85e56a6299197.jpg'}}>
+                            <Image style={{width:50,height:50}} source={{uri:'http://p0.meituan.net/280.0/groupop/bbe136d503834510784bfb15500afe5426910.png'}}>
+                            </Image>
+                        </Image>
+
+                    </View>
 
                 </View>
 
@@ -90,8 +112,8 @@ var styles = StyleSheet.create({
     viewborder:{
         borderWidth:5,
         borderColor:'green',
-        width:100,
-        height:40,
+        width:150,
+        height:100,
     },
     viewcenter:{
         alignSelf:'center',
@@ -109,6 +131,11 @@ var styles = StyleSheet.create({
         borderWidth:2,
         width:50,
         height:50,
+    },
+    img:{
+        height:80,
+        width:60,
+
     },
 
 
