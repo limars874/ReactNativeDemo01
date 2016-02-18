2f508d68eb3b41f8e543ac70a0b7f0f05a8e340b
limars874 <limars874@gmail.com>
2016-02-18 19:51:03 +0800

    添加08，测试兄弟组件的通讯，很麻烦，尽量不要这么干。

---
+++ layoutTest08.js
@@ -0,0 +1,120 @@
+/**
+ Created by yili on 1/14/16.
+ 测试兄弟组件之间通讯，实在是太麻烦了。本质是子组件调用父组件的包含有setstate方法的方法名，然后父组件把更新过的state值传给另外一个自组件进行render。
+
+ **/
+
+
+'use strict';
+
+var React = require('react-native');
+var Dimensions = require('Dimensions');
+var {Icon,} = require('react-native-icons');
+
+var {
+    View,
+    Text,
+    TextInput,
+    StyleSheet,
+    Image,
+    ScrollView,
+    TouchableOpacity,
+    Navigator,
+    ListView,
+    Component,
+    } = React;
+
+import LayoutTest06 from './layoutTest06.js';
+
+import LayoutTest07 from './layoutTest07.js';
+
+var screenW = Dimensions.get('window').width;
+
+
+
+
+
+export default class layoutTest08 extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            st:1,
+        };
+
+
+    }
+
+    handleChange(textValue){
+        this.setState({st:textValue})
+        console.log("handleChange is "+textValue);
+    }
+
+
+
+    render() {
+
+
+        return (
+            <ScrollView style={[styles.scrollview,]}>
+                <LayoutTest06  updateChange={this.handleChange.bind(this)}/>
+                <LayoutTest07 sttt = {this.state.st}/>
+
+            </ScrollView>
+        );
+    }
+
+
+
+
+};
+
+
+var styles = StyleSheet.create({
+    container: {
+        width:120,
+        height:220,
+        flexWrap:'wrap',
+        flexDirection: 'column',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+        marginLeft:25,
+        marginTop:0,
+
+
+    },
+    rightContainer: {
+        flex: 1,
+        alignItems:'center',
+    },
+    title: {
+        fontSize: 8,
+        marginBottom: 8,
+        textAlign: 'center',
+    },
+    year: {
+        textAlign: 'center',
+    },
+    thumbnail: {
+        width: 106,
+        height: 162,
+    },
+    listView: {
+        paddingTop: 20,
+        backgroundColor: '#F5FCFF',
+        flexDirection:'row',
+        flexWrap:'wrap',
+        width:screenW,
+    },
+
+    borderall: {
+        borderWidth: 1,
+        borderColor: 'red',
+    },
+
+
+
+
+
+});
+
+module.exports = layoutTest08;
